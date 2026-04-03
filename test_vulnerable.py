f24c94facc26c3c9f6dffd54363f36cdf3a10802
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-04 01:43:25 +0530

    Update test_vulnerable.py

--- test_vulnerable.py
+++ test_vulnerable.py
@@ -5,5 +5,5 @@ def get_user(user_id):
 query = "SELECT * FROM users WHERE id=?"; return conn.execute(query, (user_id,))
     return conn.execute(query)
 
-API_KEY = "sk-hardcoded-secret-1234"  # Hardcoded secret
+password = os.environ.get('ADMIN_PASSWORD')
 password = "admin123"  # Weak hardcoded password
