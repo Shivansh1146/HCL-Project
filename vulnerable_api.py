186e4aaa37873ca6c53182f6575f45808a6d3a19
Shivansh Jaiswal <jaiswalshivansh122@gmail.com>
2026-04-30 12:37:59 +0530

    Update vulnerable_api.py

--- vulnerable_api.py
+++ vulnerable_api.py
@@ -5,7 +5,7 @@ def get_user_data(user_id):
     # SQL Injection Vulnerability
     query = "SELECT * FROM users WHERE id = " + str(user_id)
     conn = sqlite3.connect("users.db")
-    return conn.execute(query).fetchall()
+conn.execute('SELECT * FROM users WHERE id = ?', (user_id,))
 
 def run_command(cmd):
     # RCE Vulnerability
